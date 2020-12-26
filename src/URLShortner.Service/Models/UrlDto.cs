ff6326a491f0126ab50c7aeb04ed277cd2d8fbef
YauheniyaSirotkina <zsirotkina@gmail.com>
2020-12-26 17:58:52 +0300

    added dto model

---
+++ src/URLShortner.Service/Models/UrlDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace URLShortner.Service.Models
+{
+    public class UrlDto
+    {
+        public int UrlId { get; set; }
+
+        public string LongUrl { get; set; }
+
+        public string ShortUrl { get; set; }
+
+        public int Hits { get; set; }
+
+        public DateTime GeneratedDate { get; set; }
+    }
+}
