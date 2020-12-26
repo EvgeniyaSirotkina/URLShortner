6c3e95d6a2fa5c48cb5a8a63584c02a58400ddb0
YauheniyaSirotkina <zsirotkina@gmail.com>
2020-12-26 18:09:08 +0300

    add migration

---
+++ src/URLShortner.Web/Migrations/20201226143953_InitialDb.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace URLShortner.Web.Migrations
+{
+    public partial class InitialDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Urls",
+                columns: table => new
+                {
+                    UrlId = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    LongUrl = table.Column<string>(nullable: false),
+                    ShortUrl = table.Column<string>(nullable: false),
+                    Hits = table.Column<int>(nullable: false),
+                    GeneratedDate = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Urls", x => x.UrlId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Urls");
+        }
+    }
+}
