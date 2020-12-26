a1f5c81fdff09f56aec33da028b09b5e0e20d68b
YauheniyaSirotkina <zsirotkina@gmail.com>
2020-12-26 17:59:29 +0300

    added constants

---
+++ src/URLShortner.Service/Infrastructure/Constants.cs
@@ -0,0 +1,30 @@
+﻿namespace URLShortner.Service.Infrastructure
+{
+    /// <summary>
+    /// Constants values that are often used.
+    /// </summary>
+    public class Constants
+    {
+        public const string UNKNOWN = "Unknown";
+
+        /// <summary>
+        /// Hypertext transfer protocols.
+        /// </summary>
+        public class Protocol
+        {
+            public const string HTTP = "http://";
+
+            public const string HTTPS = "https://";
+        }
+
+        /// <summary>
+        /// Difference for symbol definition for ASCII Table.
+        /// </summary>
+        public class ASCIITable
+        {
+            public const int ADDITIVE_GET_NUMBER = 48;
+
+            public const int ADDITIVE_GET_CHARACTER = 87;
+        }
+    }
+}
