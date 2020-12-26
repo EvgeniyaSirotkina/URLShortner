60ae885f0cac07f6fc716d8e58737f0ccbe81ea5
YauheniyaSirotkina <zsirotkina@gmail.com>
2020-12-26 18:00:33 +0300

    added summary for public members

--- src/URLShortner.Service/Models/UrlDto.cs
+++ src/URLShortner.Service/Models/UrlDto.cs
@@ -2,16 +2,34 @@
 
 namespace URLShortner.Service.Models
 {
+    /// <summary>
+    /// Data transfer object for Url model.
+    /// </summary>
     public class UrlDto
     {
+        /// <summary>
+        /// Unique identifier for Url object.
+        /// </summary>
         public int UrlId { get; set; }
 
+        /// <summary>
+        /// Provided Url.
+        /// </summary>
         public string LongUrl { get; set; }
 
+        /// <summary>
+        /// Generated short Url.
+        /// </summary>
         public string ShortUrl { get; set; }
 
+        /// <summary>
+        /// Number of clicks on a short link.
+        /// </summary>
         public int Hits { get; set; }
 
+        /// <summary>
+        /// Short Url generation date.
+        /// </summary>
         public DateTime GeneratedDate { get; set; }
     }
 }
